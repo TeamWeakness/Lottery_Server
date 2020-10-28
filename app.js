5c3b8e92a0ae2f1f57ccaa41904da0c6951be036
Kyeongwon Lee <aelleek@gmail.com>
2020-10-28 14:50:20 +0900

    Add dotenv module and Update app.js

--- app.js
+++ app.js
@@ -3,6 +3,14 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var dotenv = require('dotenv');
+
+
+/**
+ * Load environment variables from .env file, where API keys and passwords are configured.
+ */
+dotenv.config({ path: '.env.dev' });
+
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
